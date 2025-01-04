2140bcd36a4999d64eadfae11480ab7225906cfb
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-04 17:35:52 +0530

    feat: add ResponseHandler utility for error handling and response validation

---
+++ src/logic/Utils/ResponseHandler.ts
@@ -0,0 +1,33 @@
+class ResponseError extends Error {
+    status: number;
+    bodyMessage: number;
+    constructor(code: number, bodyMessage: number) {
+      super();
+      this.status = code;
+      this.bodyMessage = bodyMessage;
+    }
+  }
+  
+  export async function CheckResponse(
+    res: Response,
+    mustCode: number
+  ): Promise<{ status: number; body: any; message: string | null }> {
+    const body = await res.json();
+    if (res.status !== mustCode) {
+      console.log(res.status, "Status Code");
+      throw new ResponseError(res.status, body.message || "");
+    }
+    if (body.is_error) {
+      console.log(body.message, "Body Message");
+      throw new ResponseError(res.status, body.message || "");
+    }
+    return { status: res.status, body, message: body.message };
+  }
+  
+  export function ThrowFor(err: unknown, errFor: { [key: number]: string }) {
+    const error: { status: number; message: string } = {
+      status: (err as any).status,
+      message: (err as any).message || "",
+    };
+    return new Error(errFor[error.status] || "Something went wrong.");
+  }
