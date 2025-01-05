743f8e2887ef233571bd4334e90c776856a3826e
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-05 19:57:05 +0530

    feat: add Profile management with MProfile interface, ProfileRepo, and ProfileStore for user data handling

---
+++ src/logic/Repo/ProfileRepo.ts
@@ -0,0 +1,25 @@
+import { MProfile } from "../Model/MProfile";
+import { AuthHeaders, Request } from "../Utils/Fetch";
+import { CheckResponse, ThrowFor } from "../Utils/ResponseHandler";
+
+export class ProfileRepo {
+    baseUrl: string;
+    rq: Request;
+    constructor(baseUrl: string, rq: Request) {
+        this.rq = rq;
+        this.baseUrl = baseUrl;
+    }
+
+    async getProfile(token: string): Promise<MProfile> {
+        try {
+            const res = await this.rq.Get(`${this.baseUrl}`, AuthHeaders(token));
+            const { body } = await CheckResponse(res, 200);
+            const profile = body as MProfile;
+            return profile;
+        } catch (err: any) {
+            throw ThrowFor(err, {
+                404: "No such profile exists.",
+            });
+        }
+    }
+}
