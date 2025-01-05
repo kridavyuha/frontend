c80cf519ca95e17f82d560bed56329a650b6d967
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-05 19:58:17 +0530

    feat: enhance LoginScreen with authentication state handling and navigation

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,44 +1,84 @@
-import React, { useState } from 'react';
-import { Card, TextInput, PasswordInput, Button } from '@mantine/core';
+import React, { useEffect, useState } from 'react';
+import { Card, TextInput, PasswordInput, Button, Loader } from '@mantine/core';
 import { useStores } from '../../logic/Providers/StoreProviders';
+import { useNavigate } from 'react-router-dom';
 
-const LoginScreen: React.FC = () => {
+import { observer } from 'mobx-react-lite';
+import { Spinner } from '../../components/Spinner';
+
+const AUTH_INITIAL = 0;
+const CHECKING_AUTH = 1;
+const CHECKED_AUTH_LOGGED_IN = 2;
+
+const LoginScreen: React.FC = observer(() => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const {authStore} = useStores();
+    const { authStore } = useStores();
+    const navigate = useNavigate();
+
+
+    const [authStage, setAuthStage] = useState(CHECKING_AUTH);
+
+    useEffect(() => {
+        const checkLoginStatus = async () => {         
+            const token: string = await authStore.isLoggedIn()
+            if (token !== "") {
+                setAuthStage(CHECKED_AUTH_LOGGED_IN);
+            }
+            else {
+                setAuthStage(AUTH_INITIAL);
+            }
+        
+        };
+        checkLoginStatus();
+    }, []);
+
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
         await authStore.login(username, password);
-        window.location.href = `/leagues`;
-
+        navigate('/leagues');
     };
 
-    return (
-        <Card shadow="sm" padding="lg">
-            <form onSubmit={handleSubmit}>
-                <TextInput
-                    label="Username"
-                    placeholder="Enter your username"
-                    value={username}
-                    onChange={(event) => setUsername(event.currentTarget.value)}
-                    required
-                />
-                <PasswordInput
-                    label="Password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(event) => setPassword(event.currentTarget.value)}
-                    required
-                    mt="md"
-                />
-                <Button type="submit" fullWidth mt="xl">
-                    Submit
-                </Button>
-            </form>
-        </Card>
-    );
-};
+    if (authStage === CHECKED_AUTH_LOGGED_IN) {
+        navigate("/leagues");
+    }
+
+    if (authStage === CHECKING_AUTH) {
+        return (
+         <Spinner/>
+        );
+    }
+    
+    if (authStage === AUTH_INITIAL) {
+        return (
+            <Card shadow="sm" padding="lg">
+                <form onSubmit={handleSubmit}>
+                    <TextInput
+                        label="Username"
+                        placeholder="Enter your username"
+                        value={username}
+                        onChange={(event) => setUsername(event.currentTarget.value)}
+                        required
+                    />
+                    <PasswordInput
+                        label="Password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(event) => setPassword(event.currentTarget.value)}
+                        required
+                        mt="md"
+                    />
+                    <Button type="submit" fullWidth mt="xl">
+                        Submit
+                    </Button>
+                </form>
+            </Card>
+        );
+    }
+    
+});
+
 
 export default LoginScreen;
