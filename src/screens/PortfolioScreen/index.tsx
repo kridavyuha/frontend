5774d24ea01e1e43aacce402ab5697101148925b
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-09 13:39:18 +0530

    feat: integrate PortfolioStore into PortfolioScreen for improved portfolio management

--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -1,6 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import { Badge, Card,Text } from '@mantine/core';
 import React, { useEffect, useState } from 'react';
+import { useStores } from '../../logic/Providers/StoreProviders';
+import { PortfolioCard } from './PortfolioCard';
 
 
 interface Players {
@@ -18,60 +20,36 @@ interface Share {
 
 
 export const PortfolioScreen: React.FC = () => {
-    
-    const [userShares, setUserShares] = useState<Share>();
 
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
-    const leagueId = searchParams.get('leagueId');
+    const leagueId = searchParams.get('leagueId') || '';
+    const {portfolioStore} = useStores();   
+    const {portfolio} = portfolioStore;
 
     useEffect(() => {
         const fetchPortfolio = async () => {
-            try {
-                const response = await fetch(`http://localhost:8080/portfolio?league_id=${leagueId}`, {
-                    headers: {
-                        'Authorization': `Bearer ${localStorage.getItem('token')}`
-                    }
-                });
-                if (response.ok) {
-                    const data: Share = await response.json();
-                    setUserShares(data);
-                } else {
-                    console.error('Failed to fetch portfolio');
-                }
-            } catch (error) {
-                console.error('Error fetching portfolio:', error);
-            }
+           await portfolioStore.getPortfolio(leagueId);
         };
 
         fetchPortfolio();
     }, []);
+
+    // add a loader ..
     
 	return (
         <div>
             <div style={{ textAlign: 'center' }}>
                 <Text>Portfolio</Text>
             </div>
-            {userShares ? (
+            {portfolio ? (
                 <div>
                     <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
-                        <Text>Balance: {userShares.balance}</Text>
+                        <Text>Balance: {portfolio.balance}</Text>
                     </div>
                     <div>
-                        {userShares.players.map(player => (
-                            <Card key={player.player_id} shadow="sm" padding="lg" style={{ marginBottom: '1rem' }}>
-                                
-                                <div style={{ display: 'flex', justifyContent: 'space-between', fontFamily: 'Times New Roman, Times, serif' }}>
-                                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                                        <Badge>{player.team_name}</Badge>
-                                        <Text>{player.player_name}</Text>
-                                    </div>
-                                    <div>
-                                        <Text>Shares: {player.shares}</Text>
-                                        <Text>Current Price: {player.cur_price}</Text>
-                                    </div>
-                                </div>
-                            </Card>
+                        {portfolio.players.map(player => (
+                            <PortfolioCard key={player.player_id} player={player} />
                         ))}
                     </div>
                 </div>
