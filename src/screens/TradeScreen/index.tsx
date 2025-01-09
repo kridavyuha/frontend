4a81a25988752dda4d78d004ee2a2d42be578948
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-09 23:57:25 +0530

    feat: refactor TradeScreen to handle WebSocket messages and improve navigation

--- src/screens/TradeScreen/index.tsx
+++ src/screens/TradeScreen/index.tsx
@@ -3,14 +3,34 @@ import { Button } from '@mantine/core';
 import { LiaPiggyBankSolid } from 'react-icons/lia';
 import CardLayout from './PlayerCard';
 import { useWebSocket } from '../../hooks/useWebSocket';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import { useStores } from '../../logic/Providers/StoreProviders';
 import { Spinner } from '../../components/Spinner';
+import { MTradeEntity } from '../../logic/Model/MTrade';
+import { notifications } from '@mantine/notifications';
 
 
-const handlePortfolio = (leagueId: string | null) => {
-    window.location.href = `/portfolio?leagueId=${leagueId}`;
+const handleWSMessage = (messages : any, entities: MTradeEntity[]| null) => {
+    const updatedPlayers: MTradeEntity[] = (entities || []).map((player) => {
+        const message: number = messages[0].players[player.player_id];
+        if (message) {
+            // new price
+            console.log("newPrice" ,message)
+            const lastPrice = player.cur_price;
+            const curPrice = message
+            // pos or neg
+            const lastChange = player.last_change;
+            const change = curPrice > 0 ? 'pos' : curPrice < 0 ? 'neg' : lastChange;
+            return {
+                ...player,
+                cur_price: lastPrice + (message),
+                last_change: change,
+            };
+        }
+        return player;
+    });
+    return updatedPlayers;
 }
 
 //TODO: when this screen is being called, things are getting rendered multiple times.
@@ -18,51 +38,35 @@ export const TradeScreen: React.FC =  observer(() => {
 
     const [searchParams] = useSearchParams();
     const leagueId:string = searchParams.get('leagueId') || ''; 
+    const matchId:string = searchParams.get('matchId') || '';
+    const navigate = useNavigate();
 
     const { tradeStore } = useStores();
-    tradeStore.league_id = leagueId;
     const {entities} = tradeStore;
          
     const teams = Array.from(new Set((entities || []).map(card => card.team)));
     const [selectedTeam, setSelectedTeam] = useState<string | null>(null);
 
     useEffect(() => {
-        console.log("About component mounted!");
         const fetchEntities = async () => {
             await tradeStore.getEntities(leagueId);
+           tradeStore.setLeagueId(leagueId);
         };
         fetchEntities();
     }, []);
-    
 
-    // TODO: abstract the below logic
     const { isConnected, messages, sendMessage } = useWebSocket({
-        url: 'ws://localhost:8080/ws'
+        url: 'ws://localhost:8080/ws?league_id=' + leagueId + '&match_id=' + matchId,
     });
+    
 
     useEffect(() => {
         if (messages.length > 0) {
-            const updatedPlayers = (entities || []).map((player) => {
-                const message = messages[0].players[player.player_id];
-                if (message) {
-                    // new price
-                    console.log("newPrice" ,message)
-                    const lastPrice = player.cur_price;
-                    const curPrice = message
-                    // pos or neg
-                    const lastChange = player.last_change;
-                    const change = curPrice > 0 ? 'pos' : curPrice < 0 ? 'neg' : lastChange;
-                    return {
-                        ...player,
-                        cur_price: lastPrice + (message),
-                        last_change: change,
-                    };
-                }
-                return player;
-            });
+            const updatedPlayers: MTradeEntity[] = handleWSMessage(messages, entities);
             tradeStore.setEntities(updatedPlayers);
         }
     }, [messages]);
+
 
     if (tradeStore.isLoading === true) {
         return (
@@ -85,7 +89,7 @@ export const TradeScreen: React.FC =  observer(() => {
                         ))}
                     </div>
                     <div className="ml-auto">
-                        <LiaPiggyBankSolid size={36} onClick={() => handlePortfolio(leagueId)} />
+                        <LiaPiggyBankSolid size={36} onClick={() =>  navigate(`/portfolio?leagueId=${leagueId}`)} />
                     </div>
                 </div>
                 <div className="grid grid-cols-1 gap-4 mt-4">
