a42d085f8cf924b6e422ab157fa7894a4c38d99b
Rithvik89 <rithvikalkanti@gmail.com>
2025-01-17 22:11:20 +0530

    feat: extend MProfile to include rating and credits, and create MCompleteProfile interface; update ProfileRepo and ProfileStore to reflect changes

--- src/logic/Model/MProfile.ts
+++ src/logic/Model/MProfile.ts
@@ -1,6 +1,16 @@
+import { MLeague } from "./MLeague";
+
 export interface MProfile {
   user_id: number;
   user_name: string;
   mail_id: string;
   profile_pic: number;
+  rating: number;
+  credits: number;
 }
+
+export interface MCompleteProfile {
+  profile: MProfile;
+  leagues: MLeague[];
+}
+
